b9126061813ed9abfe12cf1f09cea6915b46e98c
suyashjawale <58392029+suyashjawale@users.noreply.github.com>
2020-04-13 16:32:02 +0530

    Update jarvis.sh

--- jarvis.sh
+++ jarvis.sh
@@ -9,12 +9,17 @@ else
 	echo "No Internet Connection"
 	exit 1
 fi
-
+echo "This may take some time. Now sit back and relax."
+echo "Installing essentials..."
 pkg install python -y >/dev/null 2>&1
+echo "Installing wget..."
 pkg install wget -y >/dev/null 2>&1
+echo "Installing ffmpeg..."
 pkg install ffmpeg -y >/dev/null 2>&1
+echo "Installing termux-api..."
 pkg install termux-api -y >/dev/null 2>&1
 command -v python > /dev/null 2>&1 || { echo >&2 "Python not installed"; exit 1; }
 command -v wget > /dev/null 2>&1 || { echo >&2 "Wget not installed"; exit 1; }
 command -v ffmpeg > /dev/null 2>&1 || { echo >&2 "Voice Module Not Installed"; exit 1; }
-python hello.py
+echo "Installing modules..."
+python install.py
