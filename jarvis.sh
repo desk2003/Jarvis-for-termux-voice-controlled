738f82ab1fa9c09a7e1c6bdde23e9b2c550c952e
suyashjawale <58392029+suyashjawale@users.noreply.github.com>
2020-04-13 13:59:29 +0530

    Add files via upload

---
+++ jarvis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Javis A.I --JDC
+#youtube.com/suyash Jawale
+#not for illegel use
+#Made By Suyash Jawale.
+if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+	echo ""
+else
+	echo "No Internet Connection"
+	exit 1
+fi
+pkg install python -y >/dev/null 2>&1
+pkg install wget -y >/dev/null 2>&1
+pkg install ffmpeg -y >/dev/null 2>&1
+pkg install termux-api -y >/dev/null 2>&1
+pip install youtube-dl -y  > /dev/null 2>&1
+pip install bs4 -y  > /dev/null 2>&1
+pip install --upgrade pip -y  > /dev/null 2>&1
+command -v python > /dev/null 2>&1 || { echo >&2 "Python not installed"; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 "Wget not installed"; exit 1; }
+command -v ffmpeg > /dev/null 2>&1 || { echo >&2 "Python Not Installed"; exit 1; }
+python hello.py
