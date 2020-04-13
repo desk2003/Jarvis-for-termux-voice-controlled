e2ebbfb839e46b280b4ab1272f22e88b48ac5980
suyashjawale <58392029+suyashjawale@users.noreply.github.com>
2020-04-13 14:59:56 +0530

    Add files via upload

---
+++ install.py
@@ -0,0 +1,2 @@
+import os
+os.system("pip install youtube-dl;pip install bs4;pip install --upgrade pip;pip install requests;python hello.py")
---
+++ jarvis.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Javis A.I --JDC
+#youtube.com/suyash Jawale
+#not for illegel use
+#Made By Suyash Jawale.
+if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+	echo "Installing Please Wait..."
+else
+	echo "No Internet Connection"
+	exit 1
+fi
+
+pkg install python -y >/dev/null 2>&1
+pkg install wget -y >/dev/null 2>&1
+pkg install ffmpeg -y >/dev/null 2>&1
+pkg install termux-api -y >/dev/null 2>&1
+command -v python > /dev/null 2>&1 || { echo >&2 "Python not installed"; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 "Wget not installed"; exit 1; }
+command -v ffmpeg > /dev/null 2>&1 || { echo >&2 "Voice Module Not Installed"; exit 1; }
+python hello.py
